75f5504950b7d6e2436cc2c29e63fdb53c9ab2d9
Sage Davies <sagelovestheforest@gmail.com>
2025-07-01 22:06:16 +0000

    Is this what you're looking for: const firebaseConfig = {   apiKey: "AIz

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,13 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Replace with your app's Firebase project configuration
-// In your Firebase project console, go to Project Settings > General and find your web app.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyC1hou9OBPyYAkX1Cek1BvyAITARkhCzDE",
+  authDomain: "eqpmgr-f82e7.firebaseapp.com",
+  projectId: "eqpmgr-f82e7",
+  storageBucket: "eqpmgr-f82e7.appspot.com",
+  messagingSenderId: "111719283783",
+  appId: "1:111719283783:web:5e83cc7e9ba8c8f6e84562"
 };
 
 // Initialize Firebase
