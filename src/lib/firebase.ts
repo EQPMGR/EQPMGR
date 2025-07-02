bf300436b11373778802eb718b349ee651bec48d
Sage Davies <sagelovestheforest@gmail.com>
2025-07-02 15:55:41 +0000

    Load time problem persists. App give warning that I'm operating offline.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableIndexedDbPersistence, CACHE_SIZE_UNLIMITED } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC1hou9OBPyYAkX1Cek1BvyAITARkhCzDE",
@@ -18,5 +18,21 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const storage = getStorage(app);
 const db = getFirestore(app);
+
+// Enable offline persistence
+try {
+    enableIndexedDbPersistence(db, {
+        cacheSizeBytes: CACHE_SIZE_UNLIMITED
+    }).catch((err) => {
+        if (err.code === 'failed-precondition') {
+            console.warn('Multiple tabs open, persistence can only be enabled in one tab at a time.');
+        } else if (err.code === 'unimplemented') {
+            console.warn('The current browser does not support all of the features required to enable persistence.');
+        }
+    });
+} catch (error) {
+    console.error("Error enabling firestore persistence", error);
+}
+
 
 export { app, auth, storage, db };
