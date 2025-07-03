11fee3d4fb54f2d3881fa406ff54757280c9c6be
Sage Davies <sagelovestheforest@gmail.com>
2025-07-03 04:51:01 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  allowedDevOrigins: ['https://6000-firebase-studio-1751254405660.cluster-pb4ljhlmg5hqsxnzpc56r3prxw.cloudworkstations.dev'],
+  allowedDevOrigins: ['https://6000-firebase-studio-1751254405660.cluster-pb4ljhlmg5hqsxnzpc56r3prxw.cloudworkstations.dev/'],
   webpack: (config, { isServer }) => {
     // Fix for Genkit's dependency on handlebars
     config.module.rules.push({
