72825acbe96a192340378ee2a0d6c10634c81d8b
Sage Davies <sagelovestheforest@gmail.com>
2025-07-06 16:03:22 +0000

    Perfect. Please add "Track" to bike type. I imagine that above Bike Type

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const BIKE_TYPES = ['Road', 'Time Trial', 'Triathlon', 'Cyclocross', 'Gravel', 'Cross Country', 'All Mountain', 'Enduro', 'Dirt Jumper', 'Downhill', 'Hybrid'] as const;
+export const BIKE_TYPES = ['Road', 'Track', 'Time Trial', 'Triathlon', 'Cyclocross', 'Gravel', 'Cross Country', 'All Mountain', 'Enduro', 'Dirt Jumper', 'Downhill', 'Hybrid'] as const;
 export type BikeType = (typeof BIKE_TYPES)[number];
 
 export const MOUNTAIN_BIKE_TYPES: BikeType[] = ['Cross Country', 'All Mountain', 'Enduro', 'Downhill', 'Dirt Jumper'];
