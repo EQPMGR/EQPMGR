b365c75953663b1d65400b7fddc4707b0d71fa5c
Sage Davies <sagelovestheforest@gmail.com>
2025-07-22 16:19:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/equipment/[id]/[system]/[componentId]/page.tsx
+++ src/app/(app)/equipment/[id]/[system]/[componentId]/page.tsx
@@ -86,7 +86,7 @@ export default function ComponentDetailPage() {
       }
       try {
         await updateUserComponentAction({
-          userId,
+          userId: user.uid,
           equipmentId: params.id as string,
           userComponentId: component.userComponentId,
           updatedData: {
