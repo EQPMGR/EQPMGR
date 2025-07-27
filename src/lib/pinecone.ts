e43abf9c7902ee657a03e4bce04184e8172cf181
Sage Davies <sagelovestheforest@gmail.com>
2025-07-27 14:57:44 +0000

    Same error. I set up an index on Pinecone. Here is the info: https://eqp

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -3,23 +3,23 @@
 
 import { Pinecone } from '@pinecone-database/pinecone';
 
-const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
+const getPineconeClient = () => {
+  const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
 
-if (!PINECONE_API_KEY) {
-    throw new Error("Pinecone API key is not set. Vector search functionality will be disabled.");
-}
+  if (!PINECONE_API_KEY) {
+    throw new Error("Pinecone API key is not set. Please add it to your .env file. Vector search functionality will be disabled.");
+  }
 
-const pinecone = new Pinecone({
-  apiKey: PINECONE_API_KEY,
-});
+  return new Pinecone({
+    apiKey: PINECONE_API_KEY,
+  });
+};
+
+const pinecone = getPineconeClient();
 
 // The name of the index in your Pinecone project.
 // IMPORTANT: You must create an index with this name in the Pinecone console.
 // The index should have a dimension of 768 for Google's text-embedding-004 model.
 const indexName = 'eqpmgr-components';
-
-if (!indexName) {
-  throw new Error('PINECONE_INDEX environment variable is not set.');
-}
 
 export const pineconeIndex = pinecone.index(indexName);
