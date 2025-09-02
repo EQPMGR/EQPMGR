8056223b74772a26ec4edfa6bd125e10fdc44773
Sage Davies <sagelovestheforest@gmail.com>
2025-09-02 08:00:02 +0000

    fuuuuuck

--- src/lib/firebase/clientApp.ts
+++ src/lib/firebase/clientApp.ts
@@ -1,19 +1,5 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "eqpmgr-test.firebaseapp.com",
-  projectId: "eqpmgr-test",
-  storageBucket: "eqpmgr-test.firebasestorage.app",
-  messagingSenderId: "298785422478",
-  appId: "1:298785422478:web:083473c4625749ef24dddf",
-};
-
-let app;
-if (getApps().length === 0) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApp();
-}
-
-export { app };
+export const db = getFirestore();
+export const auth = getAuth();
